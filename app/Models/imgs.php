3691c01f19fa033788bbb8e8f38e1fc3f56ea522
rubegine <rubenrecolons2001@ginebro.cat>
2022-05-24 19:03:40 +0200

    productes s'afegeixen dinamicament

--- app/Models/imgs.php
+++ app/Models/imgs.php
@@ -10,4 +10,9 @@ class imgs extends Model
     use HasFactory;
     protected $table = 'imgs';
     protected $fillable = ['url', 'id_item'];
+
+    public function item() {
+        return $this->belongsTo(items::class, 'id_item', 'id');
+    }
+
 }
