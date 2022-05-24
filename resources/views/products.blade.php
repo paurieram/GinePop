11e6c8ca777948bc1be505af4a814dc192a83c8f
joseprecolons <joseprecolons2000@ginebro.cat>
2022-05-24 15:47:16 +0200

    cards fixed

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,7 +4,7 @@
     <div class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-10 offset-1 my-5">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 col-10 col-md-12 col-lg-12 offset-1 offset-md-0 offset-lg-0 g-4">
             <div class="col">
-                <div class="card">
+                <div class="card card-products">
                 <img src="img/1.png" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="h3 card-title">500 €</h3>
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card card-products">
                 <img src="img/2.jpg" class="card-img-top" alt="...">
                 <div class="z card-body">
                     <h3 class="h3 card-title">17 €</h3>
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card card-products">
                 <img src="img/3.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="h3 card-title">69 €</h3>
@@ -31,7 +31,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card card-products">
                 <img src="img/4.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="h3 card-title">9 €</h3>
@@ -40,7 +40,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card card-products">
                 <img src="img/5.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="h3 card-title">12 €</h3>
